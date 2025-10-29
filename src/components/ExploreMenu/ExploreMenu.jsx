fe3247d7391649548e542570c179302985aa1c40
Ibraheem Shaheen <he.mo234@yahoo.com>
2025-10-29 06:37:03 +0300

    Update FoodDisplay and ExploreMenu components
    - Enhanced FoodDisplay component functionality
    - Improved ExploreMenu component structure
    - Code optimizations and improvements

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -5,7 +5,7 @@ import MenuList from "../MenuList/MenuList";
 const ExploreMenu = ({ category, setCategory }) => {
   return (
     <div className="explore-menu" id="explore-menu">
-      <h1>Explore Our Menu</h1>
+      <h1>استكشف قائمتنا</h1>
       {/* <p className="explore-menu-text">
         Choose from a diverse menu featuring a delectable array of dishes. Our
         mission is to satisfy your cravings and elevate your dining experience,
